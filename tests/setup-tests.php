4400587f10b8972963b9b000ab6ffdf457357b21
Orestes Sanchez <orestes.sanchezbenavente@telefonica.com>
2020-03-24 19:26:49 +0100

    Update with local database setup and cache for testing.

--- tests/setup-tests.php
+++ tests/setup-tests.php
@@ -13,9 +13,34 @@ if (!defined("__CA_LOCAL_CONFIG_DIRECTORY__")) {
 
 // If you require any overrides in setup.php that are specific to running unit tests, put them here.
 
+
+if (!defined("__CA_DB_HOST__")) {
+	define("__CA_DB_HOST__", 'localhost');
+}
+
+# __CA_DB_USER__ = Database login user name
+#
+if (!defined("__CA_DB_USER__")) {
+	define("__CA_DB_USER__", 'ca');
+}
+
+# __CA_DB_PASSWORD__ = Database login password
+#
+if (!defined("__CA_DB_PASSWORD__")) {
+	define("__CA_DB_PASSWORD__", '1234ca5678');
+}
+
+# __CA_DB_DATABASE__ = The name of your CollectiveAccess database
+#
+if (!defined("__CA_DB_DATABASE__")) {
+	define("__CA_DB_DATABASE__", 'collectiveaccess');
+}
+
 if (!defined("__CA_DB_TYPE__")) {
 	define("__CA_DB_TYPE__", 'mysqli');
 }
+
+define('__CA_CACHE_BACKEND__', 'file');
 
 // Use remaining settings from main config.
 require_once(__CA_BASE_DIR__ . '/setup.php');
