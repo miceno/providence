aebebc4ab10cfbcb498a34cc593111d98c2ebc9a
Orestes Sanchez <orestes.sanchezbenavente@telefonica.com>
2020-04-01 00:57:50 +0200

    Cache database.

--- tests/database_setup.sh
+++ tests/database_setup.sh
@@ -34,3 +34,9 @@ sudo service mysql restart
 # Show variables.
 echo "Show updated MySQL server variables"
 sudo mysql -uroot -e 'show variables;'
+
+if test -e "$CACHE_DIR/$PROFILE.sql";
+then
+  echo "Found cached database. Importing...";
+  sudo mysql -uroot "$DB_NAME" < "$CACHE_DIR/$PROFILE.sql";
+fi
