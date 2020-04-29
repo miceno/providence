73a7321b5363db91b09c690b1b0ac6b2e84470b2
Orestes Sanchez <orestes.sanchezbenavente@telefonica.com>
2020-04-29 19:16:22 +0200

    Add tests for refineries.

--- tests/helpers/ImportHelpersTest.php
+++ tests/helpers/ImportHelpersTest.php
@@ -34,7 +34,52 @@ use PHPUnit\Framework\TestCase;
 require_once(__CA_APP_DIR__."/helpers/importHelpers.php");
 
 class ImportHelpersTest extends TestCase {
-	# -------------------------------------------------------
+
+    protected $data;
+    protected $item;
+    protected $attributes;
+    protected $parents;
+
+    protected function setUp(): void {
+        $this->data = [
+                1 => "Verdun",
+                2 => ['Cambrai', 'Arras'],
+                3 => 'Chateau Thierry',
+                4 => 'Somme',
+                5 => 'Popperinge',
+                6 => 'Ypres;Somme;Cambrai;Ypres;Popperinge',
+                7 => ['Antwerp', 'Dieppe|Charleois|Paschendale', 'Bruges']
+        ];
+        $this->item = [
+                'settings' => [
+                        'original_values' => [
+                                'sector_ypres','sector_somme', 'sector_cambrai'
+                        ],
+                        'replacement_values' => [
+                                'Value_Ypres', 'Value_Somme', 'Value_Cambrai'
+                        ]
+                ]
+        ];
+
+        $this->attributes = array(
+                'movement_reason' => "^1",
+                'preferred_label' => ["^3"],
+                'nonpreferred_label' => array("name" => "^5")
+        );
+
+        $this->parents = array(
+                array('idno' => '^1',
+                        'name' => '^3',
+                        'type' => 'internal',
+                        'attributes' => array(
+                                'description' => '^5'
+                        )
+                )
+        );
+
+    }
+
+    # -------------------------------------------------------
 	public function testAATMatchPeople() {
         // some real-world examples
         $vm_ret = caMatchAAT(
@@ -108,4 +153,121 @@ class ImportHelpersTest extends TestCase {
 	# -------------------------------------------------------
 
 
+    /**
+     *
+     *
+     */
+    public function testCaProcessImportItemSettingsForValue() {
+        $ps_value = '7.30.pepe';
+        $pa_item_settings = array(
+                'applyRegularExpressions' => array(
+                        array("match" => '([0-9]+)\\.([0-9]+)',
+                                "replaceWith" => "\\1:\\2"),
+                        array(
+                                "match" => "[^0-9:]+",
+                                "replaceWith" => ""
+                        )
+                )
+        );
+
+        $result = caProcessImportItemSettingsForValue($ps_value, $pa_item_settings);
+        try {
+            $this->assertSame('7:30', $result);
+        } catch (Exception $exception){
+            $this->markTestIncomplete();
+        }
+    }
+
+    public function testCaValidateGoogleSheetsUrlReturnsNullForBadUrl(){
+        $url = "http://collectiveaccess.org";
+        $result = caValidateGoogleSheetsUrl($url);
+        $this->assertNull($result);
+    }
+    public function testCaValidateGoogleSheetsUrlReturnsValidatedUrl(){
+        $url = "https://docs.google.com/file/d/";
+        $result = caValidateGoogleSheetsUrl($url);
+        $this->assertSame('https://docs.google.com/file/d/export?format=xlsx', $result);
+    }
+
+    public function testCaProcessRefineryAttributesEmptyIsNotNull(){
+        $pa_attributes = $this->attributes;
+        $pa_source_data = $this->data;
+        $pa_item = $this->item;
+        $pn_c = 0;
+        $pa_options = array();
+        $result = caProcessRefineryAttributes($pa_attributes, $pa_source_data, $pa_item, $pn_c, $pa_options);
+        $this->assertNotNull($result);
+    }
+    public function testCaProcessRefineryAttributesEmptyProducesEmptyResults(){
+        $pa_attributes = array();
+        $pa_source_data = array();
+        $pa_item = array();
+        $pn_c = 0;
+        $pa_options = array();
+        $result = caProcessRefineryAttributes($pa_attributes, $pa_source_data, $pa_item, $pn_c, $pa_options);
+        $this->assertNotNull($result);
+        $this->assertEquals(0, sizeof($result));
+    }
+    public function testCaProcessRefineryAttributesWithFileType(){
+        $this->markTestIncomplete('testCaProcessRefineryAttributesWithFileType pending test');
+    }
+    public function testCaProcessRefineryAttributesWithIndexedArray(){
+        $pa_attributes = $this->attributes;
+        $pa_source_data = $this->data;
+        $pa_item = $this->item;
+        $pn_c = 0;
+        $pa_options = array();
+        $result = caProcessRefineryAttributes($pa_attributes, $pa_source_data, $pa_item, $pn_c, $pa_options);
+        $this->assertNotNull($result);
+        $this->assertEquals($this->data[3], $result["preferred_label"][0]);
+    }
+    public function testCaProcessRefineryAttributesWithAssociativeArray(){
+        $pa_attributes = $this->attributes;
+        $pa_source_data = $this->data;
+        $pa_item = $this->item;
+        $pn_c = 0;
+        $pa_options = array();
+        $result = caProcessRefineryAttributes($pa_attributes, $pa_source_data, $pa_item, $pn_c, $pa_options);
+        $this->assertNotNull($result);
+        $this->assertEquals($this->data[5], $result["nonpreferred_label"]["name"]);
+    }
+
+
+    public function testCaProcessRefineryParentsPlacesHierarchy(){
+
+        require_once (__CA_APP_DIR__. '/refineries/placeHierarchyBuilder/placeHierarchyBuilderRefinery.php');
+
+        $stubRefinery = $this->createMock(placeHierarchyBuilderRefinery::class);
+        $stubRefinery->method('getName')->willReturn('placeHierarchyBuilderRefinery');
+        $ps_refinery_name = 'placeHierarchyBuilderRefinery';
+        $ps_table = 'ca_places';
+        $pa_parents = $this->parents;
+        $pa_source_data = $this->data;
+        $pa_item = $this->item;
+        $pn_c = 0;
+        $pa_options = array(
+                'refinery' => $stubRefinery,
+        );
+        $result = caProcessRefineryParents($ps_refinery_name, $ps_table, $pa_parents, $pa_source_data, $pa_item, $pn_c, $pa_options);
+        $this->assertNotNull($result);
+    }
+
+    public function testCaProcessRefineryParentsCollectionHierarchy(){
+
+        require_once (__CA_APP_DIR__. '/refineries/collectionHierarchyBuilder/collectionHierarchyBuilderRefinery.php');
+
+        $stubRefinery = $this->createMock(collectionHierarchyBuilderRefinery::class);
+        $stubRefinery->method('getName')->willReturn('collectionHierarchyBuilderRefinery');
+        $ps_refinery_name = 'collectionHierarchyBuilderRefinery';
+        $ps_table = 'ca_collections';
+        $pa_parents = $this->parents;
+        $pa_source_data = $this->data;
+        $pa_item = $this->item;
+        $pn_c = 0;
+        $pa_options = array(
+                'refinery' => $stubRefinery
+        );
+        $result = caProcessRefineryParents($ps_refinery_name, $ps_table, $pa_parents, $pa_source_data, $pa_item, $pn_c, $pa_options);
+        $this->assertNotNull($result);
+    }
 }
