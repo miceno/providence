36b40f6fcd31533a3d7d485883c4d613178d0629
Orestes Sanchez <orestes.sanchezbenavente@telefonica.com>
2020-05-22 16:40:32 +0200

    Update reset test db script.

--- reset_tests.sh
+++ reset_tests.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 BASEDIR=$(dirname $0)
+DUMP_FILE=${1:-$BASEDIR/tests/mysql_profile/testing.sql}
 
 # Clean cache files
 rm -rf $BASEDIR/app/tmp/collectiveaccessCache
 
-mysql -ppassword -uca_test ca_test < $BASEDIR/tests/mysql_profile/testing.sql
+mysql -ppassword -uca_test ca_test < "$DUMP_FILE"
