f795465ed9ab8c58099be1dc60838a3757134bb2
Orestes Sanchez <orestes@estotienearreglo.es>
2020-08-19 09:26:25 +0200

    Minor styling changes.

--- ConfigurationSetup.php
+++ ConfigurationSetup.php
@@ -44,18 +44,20 @@ class CaSetup {
     /**
      * Retrieve a value and return $default if there is no element set.
      *
-     * @param      $names       A dot string or an array of the elements in the path
+     * @param      $names string|array      A dot string or an array of the elements in the path
      * @param null $default
      *
      * @return mixed|null
      */
     public function get($names, $default = null) {
-        if (!is_array($names)){
+        if (!is_array($names)) {
             $names = explode('.', $names);
         }
         // To avoid infinite recursion, call parent get, since it is not overridden.
         $result = array_reduce($names,
-                function ($o, $p) use ($default) { return $o->get($p, $default); }, $this->_config);
+                function ($o, $p) use ($default) {
+                    return $o->get($p, $default);
+                }, $this->_config);
 
         return $result;
     }
@@ -64,13 +66,13 @@ class CaSetup {
      * Only allow setting of a property if $allowModifications
      * was set to true on construction. Otherwise, throw an exception.
      *
-     * @param $names    A dot string or an array of the elements in the path
+     * @param $names string|array   A dot string or an array of the elements in the path
      * @param $value
      *
      * @return $this
      */
     public function set($names, $value) {
-        if (!is_array($names)){
+        if (!is_array($names)) {
             $names = explode('.', $names);
         }
 
@@ -88,7 +90,7 @@ class CaSetup {
     /**
      *  Initialize setup, creating constants for each key matching '__CA'
      */
-    public function init(){
+    public function init() {
         $this->_toConstants();
     }
 
@@ -96,13 +98,14 @@ class CaSetup {
         $vm_all_config = $this->_config;
         foreach ($vm_all_config as $key => $value) {
             if (strpos($key, '__CA')===0) {
-                if(!defined($key)){
+                if (!defined($key)) {
                     define($key, $value);
                 }
             }
         }
     }
-};
+}
+
 
 $o_setup = new CaSetup(
         new Zend_Config_Yaml(__CA_SETUP_FILE__),
